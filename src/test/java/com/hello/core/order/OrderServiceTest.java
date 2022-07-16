289313e02d55267cfd545ae3409e1bc04f121c1a
mintaek22 <biba99@naver.com>
2022-07-16 12:45:13 +0900

    Initial commit

---
+++ src/test/java/com/hello/core/order/OrderServiceTest.java
@@ -0,0 +1,28 @@
+package com.hello.core.order;
+
+import com.hello.core.Order.Order;
+import com.hello.core.Order.OrderService;
+import com.hello.core.Order.OrderServiceImpl;
+import com.hello.core.member.Grade;
+import com.hello.core.member.Member;
+import com.hello.core.member.MemberService;
+import com.hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+
+    MemberService memberService=new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder(){
+        long memberId = 1L;
+        Member member = new Member(memberId,"memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+
+    }
+}
